3228393a14f2830777ea406d62f729b4abc210ad
Marcos Freire <marcos.freire.csdev@gmail.com>
2023-05-26 14:32:38 -0700

    Add project files.

---
+++ Microservies.Coupon.API/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace Microservies.Coupon.API.Models
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string CouponCode { get; set; } = string.Empty;
+        public double Discount { get; set; }
+        public int MinAmount { get; set; } 
+        public DateTime LastUpdated { get; set; }
+
+    }
+}
